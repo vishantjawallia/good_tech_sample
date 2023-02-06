e75123c4aa0a6ebc97a0d47593e8a529e41c2134
vishantjawallia <vishant.jawallia@gmail.com>
2023-02-07 01:01:39 +0530

    new changes

---
+++ lib/config/Paletes.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/animation.dart';
+
+class Paletes {
+  Paletes._();
+  static const Color red = Color(0xffff0302);
+  static const Color blue = Color(0xff265ffc);
+  static const Color green = Color(0xff30c957);
+  static const Color grey = Color(0xffa1a1a1);
+  static const Color black = Color(0xff000000);
+  static const Color white = Color.fromARGB(255, 255, 255, 255);
+}
---
+++ lib/widgets/SwipeButtom.dart
@@ -0,0 +1,16 @@
+// import 'package:flutter/material.dart';
+// import 'package:good_tech_sample/config/Paletes.dart';
+
+// class SwipeButtom extends StatelessWidget {
+//   final String? name;
+//   const SwipeButtom({this.name, super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       color: Paletes.red,
+//       padding: EdgeInsets.all(value),
+//       child: Text('$name'),
+//     );
+//   }
+// }
